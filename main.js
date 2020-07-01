afc291b7cad62fbfbf705efb026350702202f937
16OscarClarke <16oclarke@wakatipu.school.nz>
2020-07-01 13:30:51 +1200

    i have create a working hamburger on a test page i just need to get my nav bar to turn into it when the screen is smaller (>650px)

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-function scrollToForm() {
-  document.querySelector('#6Strings').scrollIntoView({behavior: 'smooth'});
-}
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
